a229112880fa2f2745070a68028c3dcaf7ce80b1
Liliya <lili.begisheva@gmail.com>
2015-11-07 19:20:59 +0200

    first commit

---
+++ src/com/javarush/test/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.test;
+
+/**
+ * Created by lide on 07-Nov-15.
+ */
+public class Solution {
+    public static void main (String [] args){
+
+        System.out.println("Hello World");
+    }
+
+
+
+}
